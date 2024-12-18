0e10709fe631d5d3f891865d3f79877b10fe8d44
SyunnT <register@syunn.cn>
2024-12-18 22:22:56 +0800

    [update]: initialize service api

---
+++ service/__init__.py
@@ -0,0 +1,31 @@
+class service:
+    def __init__(self):
+        self.name = None
+        self.start_command = None
+        self.running_status = None
+        self.self_starting_option = None
+        self.listen_ports = None
+        self.executable_path = None
+        self.execute_user = None
+        self.terminal = None
+
+    def source_usage(self):
+        pass
+
+    def switch_self_starting_option(self):
+        pass
+
+    def switch_running_status(self):
+        pass
+
+    def update_start_command(self):
+        pass
+
+    def get_running_status(self):
+        pass
+
+    def get_self_starting_option(self):
+        pass
+
+def get_shortlist():
+    pass
